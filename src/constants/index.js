e5c82437d5befbe9d19f56fcb07a824f94283178
BangNguyen <bangnv.bk5139@gmail.com>
2025-07-21 15:03:10 +0900

    Update music and info

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
 /**
  * Link
  */
-export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20201003T040000Z%2F20201003T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?share`;
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20250821T040000Z%2F20250821T060000Z&text=Tiệc+cưới&details=Trân+trọng+mời+bạn+đến+dự+buổi+lễ+thành+hôn+của+chúng+tôi.&location=8QJ2%2B83M%2C+Lộc+Sơn%2C+Phú+Lộc%2C+Thành+phố+Huế%2C+Vietnam`;
+export const GOOGLE_MAPS_LINK = `https://www.google.com/maps?q=8QJ2%2B83M%2C+Lộc+Sơn%2C+Phú+Lộc%2C+Huế%2C+Vietnam`;
 
 /**
  * Wedding time
  */
-export const EPOCH_START_EVENT = 1613952139;
-export const EPOCH_END_EVENT = 1613952139;
-export const UTC_WEDDING_TIME = '2025-08-21:11:00:00Z';
+export const EPOCH_START_EVENT = 1763611200;
+export const EPOCH_END_EVENT = 1763618400;
+export const UTC_WEDDING_TIME = '2025-08-21T04:00:00Z';
